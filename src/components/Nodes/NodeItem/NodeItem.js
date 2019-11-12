10fa69bcc7195aa69a1d20bb14cfd8f633c400ab
Hermann Grieder <hermann.grieder@gmail.com>
2019-11-12 13:58:54 +0100

    created StyledComponents NodeItems

--- src/components/Nodes/NodeItem/NodeItem.js
+++ src/components/Nodes/NodeItem/NodeItem.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { } from './NodeItemStyles.js';
+import { Styled } from './NodeItemStyles.js';
 import { Divider, TextWithCopy } from 'rendition';
 
 const NodeItem = ({ node }) => {
 	const { name, address, pubKey } = node;
 	return (
 		// Style Container
-		<div style={{ paddingLeft: "10px" }}>
+		<Styled.Container>
 			{/* Nodename */}
-			<span style={{ fontFamily: 'Roboto', color: "#4D4F5C" }}>{name}</span>
+			<Styled.NodeName>{name}</Styled.NodeName>
 			{/* Node Address */}
-			<div style={{ margin: "auto", width: "100%", fontFamily: 'Roboto' }}>
+			<Styled.NodeAddress>
 				<span style={{ color: "#4D4F5C", display: "inline-block", boxSizing: "border-box", width: "50%" }}>Address: </span>
 				<TextWithCopy copy={address} style={{ display: "inline-block", boxSizing: "border-box", width: "50%" }}>{address}</TextWithCopy>
-			</div>
+			</Styled.NodeAddress>
 			{/* Node Pubkey */}
 			<div style={{ margin: "auto", width: "100%", fontFamily: 'Source Sans Pro' }}>
 				<span style={{ color: "#4D4F5C", display: "inline-block", boxSizing: "border-box", width: "50%" }}>PubKey: </span>
 				<TextWithCopy copy={pubKey} style={{ display: "inline-block", boxSizing: "border-box", width: "50%" }}>{pubKey}</TextWithCopy>
 			</div>
 			<Divider />
-		</div>
+		</Styled.Container>
 	)
 }
 
--- src/components/Nodes/NodeItem/NodeItemStyles.js
+++ src/components/Nodes/NodeItem/NodeItemStyles.js
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
 
-export const $ = styled.div`
-	
+const Container = styled.div`
+	padding-left: 10px;
 `;
 
+const sharedSpanStyle = styled.span`
+font-Family: 'Roboto';
+`;
+
+const NodeName = styled(sharedSpanStyle)`
+color: #4D4F5C;
+`;
+
+const NodeAddress = styled(sharedSpanStyle)`
+margin: auto;
+width: 100%;
+`;
+
+export const Styled = {
+    Container,
+    NodeName,
+    NodeAddress
+};
