da2d0b1d2d1d812634334259be2941c6da58ad91
Hermann Grieder <hermann.grieder@gmail.com>
2019-11-19 14:28:32 +0100

    changed overflow-x hidden

--- src/components/DashboardGrid/DashboardGridStyles.js
+++ src/components/DashboardGrid/DashboardGridStyles.js
@@ -3,7 +3,7 @@ import { Card } from "rendition";
 
 export const GridCard = styled(Card)`
   padding: 0px;
-  overflow-x: scroll;
+  overflow-x: hidden;
   overflow-y: scroll;
   :hover {
     box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.34);
