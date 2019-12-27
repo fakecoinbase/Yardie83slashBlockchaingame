0627fe4015fee6811b62d31637392fac99540bb4
Hermann Grieder <hermann.grieder@gmail.com>
2019-12-27 23:34:10 +0100

    changed padding

--- src/components/Signature/Signature.js
+++ src/components/Signature/Signature.js
@@ -65,7 +65,7 @@ const Signature = () => {
             style={{
               paddingLeft: "10px",
               paddingRight: "10px",
-              paddingTop: "10px"
+              paddingTop: "5px"
             }}
           >
             <LabeledInput
