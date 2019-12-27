d0159c67700674fe9fef81462ceb1f634d3970de
Hermann Grieder <hermann.grieder@gmail.com>
2019-12-28 00:12:36 +0100

    used graqhql types for objects variable

--- src/graphql/mutations/addBlock.ts
+++ src/graphql/mutations/addBlock.ts
@@ -1,32 +1,35 @@
 import { gql } from 'apollo-boost';
-
+/**
+ * Inserts a block and updates the "blocknumber" of the included already existing transactions
+ * If inserting or updating fails, the entire transaction fails
+ */
 const ADD_BLOCK = gql`
-mutation insertBlock(
-    $blockHash: String, 
-    $blockNumber: Int,
-    $blockStatus: String, 
-    $createdAt: timestamp, 
-    $difficulty: Int, 
-    $merkleRoot: String, 
-    $nonce:Int,
-    $previousBlockHash: String, 
-    $txHash: String
-  ) {
-    insert_bloxx_block(objects: 
-      {
-        blockHash: $blockHash, 
-        blockNumber: $blockNumber, 
-        blockStatus: $blockStatus, 
-        createdAt: $createdAt, 
-        difficulty: $difficulty, 
-        merkleRoot: $merkleRoot, 
-        nonce: $nonce, 
-        previousBlockHash: $previousBlockHash, 
-        }) {
-      affected_rows
-      returning {
+mutation upsertBlock(
+  $objects: [bloxx_block_insert_input!]!) {
+    insert_bloxx_block(
+      objects:$objects
+    )
+    {
+    affected_rows
+    returning {
         blockHash
-      }
+        blockNumber
+        blockStatus
+        createdAt
+        difficulty
+        merkleRoot
+        nonce
+        previousBlockHash
+        transactions {
+          blockNumber
+          inputAddress
+          outputAddress
+          signature
+          text
+          txHash
+          value
+        }
+    }
     }
   }`;
 
