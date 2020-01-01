55386c8922347e71886c2286bbac45a472a01849
Hyp-nos <ali.habbabeh@hotmail.com>
2020-01-01 17:37:39 +0100

    mining works now

--- src/services/miningService.js
+++ src/services/miningService.js
@@ -1,13 +1,22 @@
+import hash from "./hasherService";
 
+const miningService = data => {
+  let nonce;
+  console.log("nonce begining", nonce);
 
-const miningService = (data) => {
+  for (let i = 0; i < 999; i++) {
+    let hashedData = data + ":" + i;
+    if (hash(hashedData).charAt(0) == 0) {
+      //to increase difficualty to 2, please uncomment the next line'
+      //if (hash(hashedData).charAt(0) == 0 && hash(hashedData).charAt(1) == 0 ) {
+      nonce = i;
+      console.log("hash", hash(hashedData));
+      break;
+    } else continue;
+  }
+  console.log("nonce later", nonce);
 
-    let nonce;
-
-    // do stuff with data and return nonce;
-
-
-    return nonce
-}
+  return nonce;
+};
 
 export default miningService;
