92eb91db01f081756aa12fee766f5140893ea1be
Habbabeh Ali <ali.habbabeh@fhnw.ch>
2020-01-28 13:11:05 +0100

    merkl works

--- src/components/Mempool/Mempool.js
+++ src/components/Mempool/Mempool.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { } from "./MempoolStyles.js";
-import { Provider, Box, Table, TextWithCopy } from "rendition";
+import {} from "./MempoolStyles.js";
+import { Provider, Box, Table, TextWithCopy, Alert } from "rendition";
 import Title from "../util/Title/Title";
 import useSelectedTransactions from "../../customHooks/useSelectedTransactions/useSelectedlTransactions";
 import { useOnNewTransactionAddedSubscription } from "../../generated/graphql";
-import { FaRegCopy } from 'react-icons/fa'
+import { FaRegCopy } from "react-icons/fa";
 import useDataToCheck from "../../customHooks/useDataToCheck";
 
 const Mempool = () => {
@@ -25,25 +25,30 @@ const Mempool = () => {
    */
   const { data } = useOnNewTransactionAddedSubscription();
   const [dataToShow, setDataToShow] = useState();
-  const [, setSelectedTransaction] = useSelectedTransactions();
+  const [
+    selectedTransactions,
+    setSelectedTransaction
+  ] = useSelectedTransactions();
   const [, setDataToCheck] = useDataToCheck();
 
   useEffect(() => {
     if (data !== undefined) {
       const extendedData = data.bloxx_transaction;
-      extendedData.forEach(
-        transaction => {
-          transaction.pubKey = transaction.addressByInputaddress.nodePublicKey
-          transaction.dataToCheck = {
-            signedData: (transaction.inputAddress.concat(":".concat(transaction.outputAddress.concat(":".concat(transaction.value))))),
-            pubKey: transaction.pubKey,
-            signature: transaction.signature
-          }
-        }
-      );
-      setDataToShow(extendedData)
+      extendedData.forEach(transaction => {
+        transaction.pubKey = transaction.addressByInputaddress.nodePublicKey;
+        transaction.dataToCheck = {
+          signedData: transaction.inputAddress.concat(
+            ":".concat(
+              transaction.outputAddress.concat(":".concat(transaction.value))
+            )
+          ),
+          pubKey: transaction.pubKey,
+          signature: transaction.signature
+        };
+      });
+      setDataToShow(extendedData);
     }
-  }, [data])
+  }, [data]);
 
   const columns = [
     {
@@ -51,55 +56,72 @@ const Mempool = () => {
       label: "Copy to Check",
       render: value => {
         return (
-          <FaRegCopy onClick={() => setDataToCheck(value)} style={{ cursor: "pointer" }} />
-        )
+          <FaRegCopy
+            onClick={() => setDataToCheck(value)}
+            style={{ cursor: "pointer" }}
+          />
+        );
       }
     },
     {
       field: "inputAddress",
       label: "Input Address",
-      render: (value) => {
-        if (value && value.length > 20) return value.substr(0, 7) + '....' + value.substr(value.length - 8, 8);
-        return value
+      render: value => {
+        if (value && value.length > 20)
+          return (
+            value.substr(0, 7) + "...." + value.substr(value.length - 8, 8)
+          );
+        return value;
       }
     },
     {
       field: "outputAddress",
       label: "Output Address",
-      render: (value) => {
-        if (value && value.length > 20) return value.substr(0, 7) + '....' + value.substr(value.length - 8, 7);
-        return value
+      render: value => {
+        if (value && value.length > 20)
+          return (
+            value.substr(0, 7) + "...." + value.substr(value.length - 8, 7)
+          );
+        return value;
       }
     },
     {
       field: "value",
       label: "Amount",
-      render: (value) => value
+      render: value => value
     },
     {
       field: "pubKey",
       label: "Public Key",
-      render: (value) => {
-        if (value && value.length > 20) return value.substr(27, 7) + '....' + value.substr(value.length - 34, 7);
-        return value
+      render: value => {
+        if (value && value.length > 20)
+          return (
+            value.substr(27, 7) + "...." + value.substr(value.length - 34, 7)
+          );
+        return value;
       }
     },
     {
       field: "signature",
       label: "SIG(Tx)",
-      render: (value) => {
+      render: value => {
         if (value && value.length > 20) {
-          return value.substr(0, 4) + '....' + value.substr(value.length - 4, 4)
+          return (
+            value.substr(0, 4) + "...." + value.substr(value.length - 4, 4)
+          );
         }
-        return <TextWithCopy copy={value}>{value}</TextWithCopy>
+        return <TextWithCopy copy={value}>{value}</TextWithCopy>;
       }
     },
     {
       field: "txHash",
       label: "Tx Hash",
-      render: (value) => {
-        if (value && value.length > 20) return value.substr(0, 5) + '....' + value.substr(value.length - 5, 5);
-        return value
+      render: value => {
+        if (value && value.length > 20)
+          return (
+            value.substr(0, 5) + "...." + value.substr(value.length - 5, 5)
+          );
+        return value;
       }
     }
   ];
@@ -116,15 +138,15 @@ const Mempool = () => {
               // use TxHash for rowKey; because unique
               rowKey="txHash"
               onCheck={checkedItemsArray => {
-                // FIXME We have to delete the dataToCheck and the pubKey key/value that we added on top; 
+                // FIXME We have to delete the dataToCheck and the pubKey key/value that we added on top;
                 // otherwise we get a graphql error in the publish component. Not the best solution but it works for now
                 checkedItemsArray.forEach(item => {
                   delete item.pubKey;
-                  delete item.dataToCheck
-                })
-                setSelectedTransaction(checkedItemsArray)
-              }
-              }
+                  delete item.dataToCheck;
+                });
+                // TODO limit entries to four
+                setSelectedTransaction(checkedItemsArray);
+              }}
             />
           )}
         </Box>
