3bd0018d8282c63d5f24a954cce67f47a8f42eb7
Hyp-nos <ali.habbabeh@hotmail.com>
2020-01-30 15:52:33 +0100

    Merkl works, hex works

--- src/services/merklService.js
+++ src/services/merklService.js
@@ -3,7 +3,22 @@ import hash from "./hasherService";
 const merklService = (i1, i2, i3, i4) => {
   let result;
 
-  return "ali";
+  if (i1 != null && i2 != null && i3 != null && i4 != null) {
+    let first = hash(i1 + i2);
+    let second = hash(i3 + i4);
+    return hash(first + second);
+  } else if (i1 != null && i2 != null && i3 != null) {
+    let first = hash(i1 + i2);
+    let second = hash(i3);
+    return hash(first + second);
+  } else if (i1 != null && i2 != null) {
+    let first = hash(i1 + i2);
+    return first;
+  } else if (i1 != null) {
+    let first = hash(i1);
+    return first;
+  }
+  return "you didn't fill the fields correctly";
 };
 
 export default merklService;
