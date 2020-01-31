5d2d7033622eac0fe00448c39938e5feafb0055d
Hermann Grieder <hermann.grieder@gmail.com>
2020-01-31 17:43:07 +0100

    change to typescript and added user type

--- src/customHooks/useUserInfo/useUserInfo.ts
+++ src/customHooks/useUserInfo/useUserInfo.ts
@@ -1,19 +1,28 @@
 import { useState } from 'react';
 import { createStore } from 'reusable';
 
+type User = {
+	privateKey: String
+	publicKey: String,
+	address: {
+		id: String,
+		amount: Number
+	},
+}
+
 const useUserInfo = createStore(() => {
-	const [userInfo, setUserInfo] = useState({
+	const [userInfo, setUserInfo]: [User, any] = useState({
 			privateKey: "",
 			publicKey: "",
 			address: {
 				id: "",
 				amount: 0
-			},
+			}
 	})
 
 	return [
 		userInfo,
-		(userInfo) => setUserInfo(userInfo)
+		(userInfo:User) => setUserInfo(userInfo)
 	]
 });
 
