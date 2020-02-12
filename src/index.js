7e110f7f737b3b8e2b0bf976bfc906adc4adc0df
Hermann Grieder <hermann.grieder@gmail.com>
2020-02-12 14:11:17 +0100

    removed serviceworker

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import './styles/gridLayoutStyles.css'
 import './styles/resizableStyles.css'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below.
-serviceWorker.unregister();
