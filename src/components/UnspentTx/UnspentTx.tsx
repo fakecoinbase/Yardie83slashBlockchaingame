4e3ee3c72275581e96442ed987dc2fca933f1ade
Hermann Grieder <hermann.grieder@gmail.com>
2020-04-09 12:15:50 +0200

    utxo now only shosws balances > 0

--- src/components/UnspentTx/UnspentTx.tsx
+++ src/components/UnspentTx/UnspentTx.tsx
@@ -1,64 +1,64 @@
-import React, { useState, useEffect } from "react";
-import {} from "./UnspentTxStyles.js";
-import { Provider, Box, Table, TableColumn } from "rendition";
-import Title from "../util/Title/Title";
-import { useAddressSubscription } from "../../generated/graphql";
+import React, { useState, useEffect } from 'react';
+import {} from './UnspentTxStyles.js';
+import { Provider, Box, Table, TableColumn } from 'rendition';
+import Title from '../util/Title/Title';
+import { useAddressSubscription } from '../../generated/graphql';
 
 const UnspentTx = () => {
-  const [nodes, setNodes] = useState<Array<Object>>([]);
+	const [nodes, setNodes] = useState<Array<Object>>([]);
 
-  const { data } = useAddressSubscription();
+	const { data } = useAddressSubscription();
 
-  useEffect(() => {
-    if (data) {
-      let _nodes: { address?: string; amount?: number | null }[] = [];
+	useEffect(() => {
+		if (data) {
+			let _nodes: { address?: string; amount?: number | null }[] = [];
 
-      data.bloxx_address.forEach(node => {
-        if (node.id !== "undefined" && node.balance !== null) {
-          _nodes.push({
-            address: node.id,
-            amount: node.balance
-          });
-        }
-      });
-      setNodes(_nodes);
-    }
-  }, [data]);
+			data.bloxx_address.forEach((node) => {
+				if (node.id !== 'undefined' && node.balance !== null && node.balance !== undefined && node.balance > 0) {
+					_nodes.push({
+						address: node.id,
+						amount: node.balance,
+					});
+				}
+			});
+			setNodes(_nodes);
+		}
+	}, [data]);
 
-  // const data = [
-  // 	{
-  // 		input: 'v43bc6543vr46',
-  // 		output: 'fwf3tgg4fgu64',
-  // 		amount: '2',
-  // 		pubkey: '1215sadv8s',
-  // 		signature: 'asg43vt5z332',
-  // 		text: 'Some text',
-  // 		txHash: '43ct3gc4n6767'
-  // 	}
-  // ]
+	// const data = [
+	// 	{
+	// 		input: 'v43bc6543vr46',
+	// 		output: 'fwf3tgg4fgu64',
+	// 		amount: '2',
+	// 		pubkey: '1215sadv8s',
+	// 		signature: 'asg43vt5z332',
+	// 		text: 'Some text',
+	// 		txHash: '43ct3gc4n6767'
+	// 	}
+	// ]
 
-  const columns: TableColumn<any>[] = [
-    {
-      field: "address",
-      label: "Address"
-    },
-    {
-      field: "amount",
-      label: "Amount",
-      render: (value: string) => <code>{value}</code>
-    }
-  ];
+	const columns: TableColumn<any>[] = [
+		{
+			field: 'address',
+			label: 'Address',
+		},
+		{
+			field: 'amount',
+			label: 'Amount',
+			render: (value: string) => <code>{value}</code>,
+		},
+	];
 
-  return (
-    <>
-      <Title title="Unspent Transactions (UTXO)"></Title>
-      <Provider>
-        <Box m={3}>
-          <Table columns={columns} data={nodes} />
-        </Box>
-      </Provider>
-    </>
-  );
+	return (
+		<>
+			<Title title='Unspent Transactions (UTXO)'></Title>
+			<Provider>
+				<Box m={3}>
+					<Table columns={columns} data={nodes} />
+				</Box>
+			</Provider>
+		</>
+	);
 };
 
 export default UnspentTx;
